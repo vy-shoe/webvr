b63fc18c935e745aedf18927e7d081030c562648
vy-shoe <vadimulam6@gatech.edu>
2021-09-26 15:29:28 -0400

    fixed console errors regsrding dodegherdon size dimensions

--- components/intersection-spawn.js
+++ components/intersection-spawn.js
@@ -20,6 +20,7 @@
   
         // Snap intersection point to grid and offset from center.
         spawnEl.setAttribute('position', evt.detail.intersection.point);
+        spawnEl.setAttribute('dynamic-body', "mass: 0.4");
   
         // Set components and properties.
         Object.keys(data).forEach(name => {
